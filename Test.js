e1093030f2bcc6c47d91115c7c4bc288b75e5d1e
BS1594 <siraj.salehin@brainstation-23.com>
2024-06-11 17:21:36 +0600

    ArrowFunction

--- Test.js
+++ Test.js
@@ -1,3 +1,4 @@
+'use strict'
 
 // Switch Operation
 let day = "Monday";
@@ -22,3 +23,18 @@ let bill = 275;
 let tip = bill <= 500 && bill >= 50 ? bill*.15 : bill*.2;
 
 console.log(` The bill was ${bill} and the tip will be ${tip}`);
+
+// Function + Arrow function
+
+var calcAge = (birthYear) => 2024 - birthYear;
+
+console.log(calcAge(1994));
+
+var calcRetirement = (birthYear) => {
+    var age = calcAge(birthYear);
+    var retirementAge = 50- age;
+    console.log(retirementAge);
+}
+
+calcRetirement(1994)
+
