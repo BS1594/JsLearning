c9df2752fcc231bd6c479b7c7e9f6f6aea3f481a
BS1594 <siraj.salehin@brainstation-23.com>
2024-06-11 11:25:21 +0600

    SwitchCase Test.js

---
+++ Test.js
@@ -0,0 +1,15 @@
+
+let day = "Monday";
+
+switch(day){
+
+    case "Monday":
+        console.log('The day is Matched');
+        break;
+    
+    case "Satureday":
+        console.log('The day is not Matched');
+        break;
+    default:
+        console.log('Not a Valid Day')
+}
